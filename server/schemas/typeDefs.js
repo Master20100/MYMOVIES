eca7d921ed8f0c034a10fdf8d5db15cfb83d9e72
YipChiSan <zye39@yahoo.com>
2022-10-20 19:52:52 +1100

    Finish typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        favourite_movies: [Movie]!
+    }
+
+    type Movie {
+        _id: ID
+        name: String
+        description: String
+        image: String
+        rate: Number
+        trailer: String
+        live_stream_url: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+        movie(moveId: ID!): Movie
+    }
+
+    type Mutation {
+        login(username: String!, password: String!): Auth
+        addUser(username: String!, password: String!): Auth
+        addMovie(name: String!, description: String!, image: String!, rate: Number!, trailer: String!, live_stream_url: String!): Movie
+    }
+`;
+
+module.exports = typeDefs;
